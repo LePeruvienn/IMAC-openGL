271c06cbea85a4ff9de4eeac85b41a572d196da4
leperuvienn <koto91@free.fr>
2025-09-14 20:22:17 +0200

    Update TP2

--- TP2/shaders/text2D.vs.glsl
+++ TP2/shaders/text2D.vs.glsl
@@ -3,7 +3,18 @@
 layout(location = 0) in vec2 aVertexPosition;
 layout(location = 1) in vec2 aVertexTexture;
 
+uniform float uTime;
+
+mat3 rotate(float a) {
+
+	mat3 R = mat3(vec3(cos(a), sin(a), 0), vec3(-sin(a), cos(a), 0), vec3(0, 0, 1));
+
+	return R;
+}
+
 void main() {
 
-	gl_Position = vec4(aVertexPosition, 0, 1);
+	vec2 position = aVertexPosition * rotate(uTime);
+
+	gl_Position = vec4(position, 0, 1);
 }
