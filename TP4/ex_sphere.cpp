07eb44cd3843fb076e700b7ed4e2e65a3f9c30fd
leperuvienn <koto91@free.fr>
2026-01-19 20:58:01 +0100

    Setup tp4 sphere files

---
+++ TP4/ex_sphere.cpp
@@ -0,0 +1,108 @@
+#define GLFW_INCLUDE_NONE
+#include "GLFW/glfw3.h"
+#include "glad/glad.h"
+#include <glimac/Program.hpp>
+#include <glimac/FilePath.hpp>
+#include <glimac/glm.hpp>
+#include <glimac/Sphere.hpp>
+#include <cstddef>
+#include <vector>
+
+int window_width  = 800;
+int window_height = 800;
+
+static void key_callback(GLFWwindow* window, int key, int /*scancode*/, int action, int /*mods*/)
+{
+	// Si on appuie sur la touche "Q" (qwerty -> "A" en azerty)
+	if (key == GLFW_KEY_Q && action == GLFW_PRESS) {
+
+		// Fermer la fenêtre
+		glfwSetWindowShouldClose (window, GLFW_TRUE);
+	}
+}
+
+static void mouse_button_callback(GLFWwindow* /*window*/, int /*button*/, int /*action*/, int /*mods*/)
+{
+}
+
+static void scroll_callback(GLFWwindow* /*window*/, double /*xoffset*/, double /*yoffset*/)
+{
+}
+
+static void cursor_position_callback(GLFWwindow* /*window*/, double /*xpos*/, double /*ypos*/)
+{
+}
+
+static void size_callback(GLFWwindow* /*window*/, int width, int height)
+{
+	window_width  = width;
+	window_height = height;
+}
+
+int main(int /*argc*/, char** /*argv*/)
+{
+	/* Initialize the library */
+	if (!glfwInit()) {
+		return -1;
+	}
+
+	/* Create a window and its OpenGL context */
+#ifdef __APPLE__
+	/* We need to explicitly ask for a 3.3 context on Mac */
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
+
+	GLFWwindow* window = glfwCreateWindow(window_width, window_height, "TP4", nullptr, nullptr);
+
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Intialize glad (loads the OpenGL functions) */
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		return -1;
+	}
+
+	/* Hook input callbacks */
+	glfwSetKeyCallback(window, &key_callback);
+	glfwSetMouseButtonCallback(window, &mouse_button_callback);
+	glfwSetScrollCallback(window, &scroll_callback);
+	glfwSetCursorPosCallback(window, &cursor_position_callback);
+	glfwSetWindowSizeCallback(window, &size_callback);
+
+	/*********************************
+	 * HERE SHOULD COME THE INITIALIZATION CODE
+	 *********************************/
+
+	glimac::Sphere sphere(1, 32, 16);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window)) {
+
+		glClearColor(0.f, 0.f, 0.f, 0.f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/*********************************
+		* HERE SHOULD COME THE RENDERING CODE
+		*********************************/
+	
+		// on nettoie la fenêtre pour ne pas avoir le résidu du rendu précédent,
+		// Dans glClear on met le flag pour définir ce qu'on veut nettoyer
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
